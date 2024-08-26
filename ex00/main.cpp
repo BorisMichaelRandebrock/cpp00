71e506d1c9b538c230efbe2b7c648b2e2cc83e37
Boris Randebrock <brandebr@student.42barcelona.com>
2024-08-26 17:59:03 +0200

    megaphone noising..

---
+++ ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brandebr <brandebr@42barcelona.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 13:29:49 by brandebr          #+#    #+#             */
+/*   Updated: 2024/08/26 17:48:54 by brandebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype> 
+#include <string>
+
+std::string toUppercase(const std::string &str) {
+	std::string res = str;
+	for (std::string::size_type i = 0; i < res.length(); ++i) {
+		res[i] = std::toupper(res[i]); 
+	}
+	return res;
+}
+
+int	main(int argc, char *argv[]) {
+	int	i = 0;
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	} else if (argc > 1) {
+		std::string	res;
+		for (int i = 1; i < argc; i++) {
+			res += toUppercase(argv[i]);
+			if (i < argc -1) {
+				res += " ";
+			}
+		}
+		std::cout << res << std::endl;
+	}
+	return (0);
+}
